3325d77b91f8084a8bf04159cb2b5b72d088e811
murnana <murnana@users.noreply.github.com>
2024-01-17 23:48:10 +0900

    :sparkles:  OS font (#3)

--- Assets/Murnana UGUI/Samples/OSFont/OSFontListItem.cs
+++ Assets/Murnana UGUI/Samples/OSFont/OSFontListItem.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2023 murnana.
+// Copyright 2023 murnana.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // 
@@ -50,12 +50,18 @@ namespace Murnana.UGUI.Samples.OSFont
                 format: "{0} != null",
                 m_Text
             );
+            Debug.AssertFormat(
+                condition: m_Button != null,
+                context: this,
+                format: "{0} != null",
+                m_Button
+            );
 
             m_Asset = fontAsset;
 
             var faceInfo = m_Asset.TextMeshProFontAsset.faceInfo;
-            m_Text.text = $"familyName: {faceInfo.familyName}\n<size=24>styleName:{faceInfo.styleName}</size>";
-            m_Button.interactable = true;
+            m_Text!.text = $"familyName: {faceInfo.familyName}\n<size=24>styleName:{faceInfo.styleName}</size>";
+            m_Button!.interactable = true;
             m_SelectOSFont = onSelect;
         }
 
